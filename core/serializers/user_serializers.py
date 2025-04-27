3cc3a0e64a7100f1c6ba995a955b1b7c6eb6313e
leocassiosilva <leocassiosilva1234@gmail.com>
2025-04-27 11:08:54 -0300

    feat: get quantidade de seguidores

--- core/serializers/user_serializers.py
+++ core/serializers/user_serializers.py
@@ -1,9 +1,12 @@
+from core.models.relacionamento_models import Relacionamento
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    seguidores = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
@@ -12,5 +15,11 @@ class UserSerializer(serializers.ModelSerializer):
             'email',
             'first_name',
             'last_name',
+            'seguidores',  
         ]
         read_only_fields = ['id']
+    
+        def get_seguidores(self, obj):
+            """Método para obter o número de seguidores de um usuário"""
+            quantidade_seguidores = Relacionamento.objects.filter(seguindo=obj).count()
+            return quantidade_seguidores

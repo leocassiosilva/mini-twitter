598326c56046b6d30c53421d2e70010838ff3776
leocassiosilva <leocassiosilva1234@gmail.com>
2025-04-27 20:17:42 -0300

    feat: relacionamento viewsets

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
+from core.viewsets.relacionamento_viewsets import RelacionamentoViewSet
 from core.viewsets.user_viewsets import UserViewSet
 from rest_framework import routers
 
 core_router = routers.DefaultRouter()
 core_router.register(r'user', UserViewSet)
+core_router.register(r'relacionamento', RelacionamentoViewSet)
 
 
 
